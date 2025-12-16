f9974becffb4846ab8fd00ef0f612c78dbb2f3e3
Putina-Iustina <iustina-elena.putina@student.tuiasi.ro>
2025-12-16 08:36:53 +0200

    added comments ui

--- face.py
+++ face.py
@@ -5,6 +5,7 @@ import brain as b
 
 class StartScreen:
     def __init__(self, root):
+        """Initializarea ecranului principal pentru alegerea dificulatii si a dimensiunii"""
         self.root = root
         root.title("Select Sudoku size:")
 
@@ -12,25 +13,30 @@ class StartScreen:
         self.frame.pack()
 
         tk.Label(self.frame, text="Choose dimension and difficulty:", font=('Arial', 14)).pack(pady=10)
-        self.start_menu_button = tk.Menubutton(self.frame, text="Start game", relief=tk.RAISED, font=('Arial',12), padx=10, pady=5,direction='below')
-        self.start_menu_button.pack(pady=20)
-        self.start_menu = tk.Menu(self.start_menu_button, tearoff=0)
-        self.start_menu_button.config(menu=self.start_menu)
+        self.dimension_var = tk.IntVar(value=4)
+        tk.Radiobutton(self.frame, text="4x4",variable=self.dimension_var,value=4, font=('Arial', 12), padx=10, pady=5).pack(pady=5)
+        tk.Radiobutton(self.frame, text="9x9",variable=self.dimension_var, value =9, font=('Arial', 12), padx=10, pady=5).pack(pady=5)
 
-        self.start_menu.add_command(label="4x4(Easy)", command=lambda:self.start_game(4,0))
-        self.start_menu.add_command(label="4x4(Hard)", command=lambda: self.start_game(4, 1))
-        self.start_menu.add_command(label="9x9(Easy)", command=lambda: self.start_game(9, 0))
-        self.start_menu.add_command(label="9x9(Hard)", command=lambda: self.start_game(9, 1))
+        self.difficulty_var = tk.IntVar(value=0)
+        tk.Radiobutton(self.frame, text="Easy", variable=self.difficulty_var, value=0, font=('Arial', 12), padx=10, pady=5).pack(pady=5)
+        tk.Radiobutton(self.frame, text="Hard", variable=self.difficulty_var, value=1, font=('Arial', 12),  padx=10, pady=5).pack(pady=5)
 
-    def start_game(self, dimension, difficulty):
+        tk.Button(self.frame, text="Start Game", command=self.start_game, font=('Arial', 12), padx=10, pady=5).pack(pady=15)
+
+    def start_game(self):
+        """Setarea variabilelor si inceperea jocului"""
+        dimension = self.dimension_var.get()
+        difficulty=self.difficulty_var.get()
         g.set_dimension(dimension)
         g.set_difficulty(difficulty)
+
         self.frame.destroy()
         SudokuUI(self.root)
 
 
 class SudokuUI:
     def __init__(self, root):
+        """Initializarea tabelului principal pentru joc"""
         self.root = root
         self.root.title("Sudoku")
         self.cells = []
@@ -46,6 +52,7 @@ class SudokuUI:
         self.display_grid(start_board)
 
     def validate_input(self,new_value):
+        """Verificarea inputului(daca cifra pe care o punem este valida pentru jocul respectiv)"""
         if new_value == "":
             return True
         try:
@@ -59,7 +66,7 @@ class SudokuUI:
             return False
 
     def configure_menu(self):
-        """Menu bar"""
+        """Bara de meniu"""
         menu_bar = tk.Menu(self.root)
         game_menu = tk.Menu(menu_bar,tearoff=0)
 
@@ -82,21 +89,22 @@ class SudokuUI:
         self.root.config(menu=menu_bar)
 
     def exit_app(self):
-        """Close the app"""
+        """Inchiderea aplicatiei"""
         self.root.quit()
 
     def show_about(self):
-        """About the app"""
+        """Despre aplicatie"""
         about_text = "Sudoku Solver using Forward Checking Algorithm.\n\nTeam:\n- Sorina (The Brain)\n- Iustina (The Face)\n- Alex (The Glue)"
         messagebox.showinfo("About Sudoku", about_text)
 
     def generate_new(self):
-        """Generates a new puzzle"""
+        """Generarea unui nou joc"""
         new_board = g.create_valid_board_structure()
         self.display_grid(new_board)
-        messagebox.showinfo("New Puzzle", "A new Sudoku puzzle has been generated")
+        messagebox.showinfo("New Game", "A new Sudoku game has been generated")
 
     def get_current_grid_state(self):
+        """Functie ajutatoare - ia toate valorile(atat cele generate initial, cat si cele scrise de utilizator) si le returneaza sub forma de matrice pentru verificarile urmatoare """
         grid =[]
         for row in range(g.BOARD_DIMENSION):
             row_data = []
@@ -110,6 +118,7 @@ class SudokuUI:
         return grid
 
     def solve_sudoku(self):
+        """Rezolvarea jocului curent automat"""
         current_grid = self.get_current_grid_state()
         solved_board = b.solve_sudoku_forward_checking(current_grid)
 
@@ -120,7 +129,7 @@ class SudokuUI:
             messagebox.showerror("Error", "The Sudoku has no valid solution")
 
     def create_grid_entries(self):
-        """Create grids"""
+        """Desenarea casutelor pe ecran si asezarea acestora"""
         for row in range(g.BOARD_DIMENSION):
             row_entries =[]
             for col in range(g.BOARD_DIMENSION):
@@ -159,7 +168,7 @@ class SudokuUI:
             self.cells.append(row_entries)
 
     def display_grid(self,board):
-        """Displays the board values in the Entry widgets and sets initial cells to readonly"""
+        """Afisarea numerelor si blocarea casutelor care au fost completate initial"""
         self.initial_board = [row[:] for row in board]
 
         for row in range(g.BOARD_DIMENSION):
@@ -176,11 +185,13 @@ class SudokuUI:
                     entry.config(state='normal', fg='black', readonlybackground ='white', bg='white')
 
     def start_predefined_game(self,dimension, difficulty):
+        """Reincarcarea interfetei cu noile setari alese din meniu(dimensiunea si dificultatea jocului)"""
         g.set_dimension(dimension)
         g.set_difficulty(difficulty)
         self.reload_ui()
 
     def reload_ui(self):
+        """Reconstruirea tabelului si generarea unui nou joc"""
         self.grid_frame.destroy()
         self.grid_frame = tk.Frame(self.root, padx=10,pady=10)
         self.grid_frame.pack()
